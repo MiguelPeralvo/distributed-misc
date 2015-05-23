ab958d0949e4c6c604dd1ac20e8b7d55ad0a2a8b
MiguelPeralvo <miguel.peralvo@gmail.com>
2015-05-23 14:09:35 +0100

    Uberjar and Spark SQL sample with Scala.

---
+++ spark/spark-sql-uberjar/src/main/scala/org/sia/chapter03App/GHPushCounterSubmit.scala
@@ -0,0 +1,42 @@
+package org.sia.chapter03App
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SQLContext
+
+/**
+ * @author ${user.name}
+ */
+object GHPushCounterSubmit {
+
+  def main(args : Array[String]) {
+    val conf = new SparkConf()
+    val sc = new SparkContext(conf)
+    val sqlContext = new SQLContext(sc)
+    
+    val homeDir = System.getenv("HOME")
+    val ghLog = sqlContext.jsonFile(homeDir + "/Git/SparkInAction/github-archive/*.json")
+    //val ghLog = sqlContext.load(homeDir + "/sia/github-archive/*.json", "json")
+    //val ghFile = sqlContext.load("2015-03-01-0.json", "json")
+
+    val pushes = ghLog.filter("type = 'PushEvent'") 
+    val grouped = pushes.groupBy("actor.login").count
+    val ordered = grouped.orderBy(grouped("count").desc)
+
+    
+    val empLogins = sqlContext.load(homeDir + "/Git/SparkInAction/first-edition/ch03/ghEmployees.json", 
+        "json")
+    import pushes.sqlContext.implicits._
+    
+    val joined = ordered
+      .join(empLogins, $"login" === $"username", "inner")
+      .orderBy($"count".desc)
+      .select("login", "count")
+      
+    joined.save(args(0), args(1))
+    
+    
+  }
+
+}
