68e5b21ff231f718861617936f71b616ece21283
MiguelPeralvo <miguel.peralvo@gmail.com>
2015-05-30 21:13:26 +0100

    variable for path.

--- aws/download_rsgetstarted.sh
+++ aws/download_rsgetstarted.sh
@@ -1,7 +1,7 @@
-aws s3 cp s3://awssampledbeuwest1/tickit/allusers_pipe.txt /home/ubuntu/s3/redshiftgetstarted/allusers_pipe.txt
-aws s3 cp s3://awssampledbeuwest1/tickit/venue_pipe.txt /home/ubuntu/s3/redshiftgetstarted/venue_pipe.txt
-aws s3 cp s3://awssampledbeuwest1/tickit/category_pipe.txt /home/ubuntu/s3/redshiftgetstarted/category_pipe.txt
-aws s3 cp s3://awssampledbeuwest1/tickit/date2008_pipe.txt /home/ubuntu/s3/redshiftgetstarted/date2008_pipe.txt
-aws s3 cp s3://awssampledbeuwest1/tickit/allevents_pipe.txt /home/ubuntu/s3/redshiftgetstarted/allevents_pipe.txt
-aws s3 cp s3://awssampledbeuwest1/tickit/listings_pipe.txt /home/ubuntu/s3/redshiftgetstarted/listings_pipe.txt
-aws s3 cp s3://awssampledbeuwest1/tickit/sales_tab.txt /home/ubuntu/s3/redshiftgetstarted/sales_tab.txt
+aws s3 cp s3://awssampledbeuwest1/tickit/allusers_pipe.txt $S3_PATH/redshiftgetstarted/allusers_pipe.txt
+aws s3 cp s3://awssampledbeuwest1/tickit/venue_pipe.txt $S3_PATH/redshiftgetstarted/venue_pipe.txt
+aws s3 cp s3://awssampledbeuwest1/tickit/category_pipe.txt $S3_PATH/redshiftgetstarted/category_pipe.txt
+aws s3 cp s3://awssampledbeuwest1/tickit/date2008_pipe.txt $S3_PATH/redshiftgetstarted/date2008_pipe.txt
+aws s3 cp s3://awssampledbeuwest1/tickit/allevents_pipe.txt $S3_PATH/redshiftgetstarted/allevents_pipe.txt
+aws s3 cp s3://awssampledbeuwest1/tickit/listings_pipe.txt $S3_PATH/redshiftgetstarted/listings_pipe.txt
+aws s3 cp s3://awssampledbeuwest1/tickit/sales_tab.txt $S3_PATH/redshiftgetstarted/sales_tab.txt
--- aws/upload_rsgetstarted.sh
+++ aws/upload_rsgetstarted.sh
@@ -1,7 +1,8 @@
-aws s3 cp /home/ubuntu/s3/redshiftgetstarted/allusers_pipe.txt s3://datasciencestart201505/redshiftgettingstarted/allusers_pipe.txt
-aws s3 cp /home/ubuntu/s3/redshiftgetstarted/venue_pipe.txt s3://datasciencestart201505/redshiftgettingstarted/venue_pipe.txt
-aws s3 cp /home/ubuntu/s3/redshiftgetstarted/date2008_pipe.txt s3://datasciencestart201505/redshiftgettingstarted/date2008_pipe.txt
-aws s3 cp /home/ubuntu/s3/redshiftgetstarted/allevents_pipe.txt s3://datasciencestart201505/redshiftgettingstarted/allevents_pipe.txt
-aws s3 cp /home/ubuntu/s3/redshiftgetstarted/listings_pipe.txt s3://datasciencestart201505/redshiftgettingstarted/listings_pipe.txt
-aws s3 cp /home/ubuntu/s3/redshiftgetstarted/sales_tab.txt s3://datasciencestart201505/redshiftgettingstarted/sales_tab.txt
+aws s3 cp $S3_PATH/redshiftgetstarted/allusers_pipe.txt s3://datasciencestart201505/redshiftgettingstarted/allusers_pipe.txt
+aws s3 cp $S3_PATH/redshiftgetstarted/venue_pipe.txt s3://datasciencestart201505/redshiftgettingstarted/venue_pipe.txt
+aws s3 cp $S3_PATH/redshiftgetstarted/category_pipe.txt s3://datasciencestart201505/redshiftgettingstarted/category_pipe.txt
+aws s3 cp $S3_PATH/redshiftgetstarted/date2008_pipe.txt s3://datasciencestart201505/redshiftgettingstarted/date2008_pipe.txt
+aws s3 cp $S3_PATH/redshiftgetstarted/allevents_pipe.txt s3://datasciencestart201505/redshiftgettingstarted/allevents_pipe.txt
+aws s3 cp $S3_PATH/redshiftgetstarted/listings_pipe.txt s3://datasciencestart201505/redshiftgettingstarted/listings_pipe.txt
+aws s3 cp $S3_PATH/redshiftgetstarted/sales_tab.txt s3://datasciencestart201505/redshiftgettingstarted/sales_tab.txt
 
